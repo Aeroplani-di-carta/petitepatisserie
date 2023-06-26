b66d9bc7c104a9c189d011edcd2385563ea6e90c
Gaetano Vallarelli <biroplane@users.noreply.github.com>
2023-06-26 18:16:55 +0200

    Skeleton of the website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~~/assets/css/app.css"],
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+  },
+});
